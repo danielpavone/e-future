9d98e267cd9902f1fcd2694a9ef1f39ee03521d0
Daniel Pavone <danielpavone@gmail.com>
2019-11-27 11:24:54 -0300

    relationship workshop with school

--- src/models/Workshop.js
+++ src/models/Workshop.js
@@ -3,7 +3,12 @@ const mongoose = require('mongoose');
 const WorkshopSchema = new mongoose.Schema({
   name: String,
   description: String,
-  type: String
+  type: String,
+  school: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'School'
+  },
+  date: Date
 }, { timestamps: true });
 
 module.exports = mongoose.model('Workshop', WorkshopSchema);
